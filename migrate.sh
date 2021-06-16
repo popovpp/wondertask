cf9ac431c77ec4311eb858a3e00acd974486afc5
fray23 <mars6358@gmail.com>
2021-06-16 14:50:23 +0600

    tty bash migrate script fixed

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f docker-compose-prod.yml exec web bash -c "python3 manage.py showmigrations"
-docker-compose -f docker-compose-prod.yml exec web bash -c "python3 manage.py migrate"
+docker-compose -f docker-compose-prod.yml exec -T web bash -c "python3 manage.py showmigrations"
+docker-compose -f docker-compose-prod.yml exec -T web bash -c "python3 manage.py migrate"
