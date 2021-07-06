6ac1c53028b071b36c230378df965735d66cfcdd
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-07-06 22:53:17 +0300

    Updated tests and added tests for task schedule, task services, notifications and journal services

--- wondertask/tests/test_02_docs.py
+++ wondertask/tests/test_02_docs.py
@@ -23,8 +23,7 @@ def test_02_task_doc_create_without_file(user_client, create_task):
 
 
 @pytest.mark.django_db()
-def test_03_task_doc_create_with_right_file_type(user_client,
-                                                 create_task):
+def test_03_task_doc_create_with_right_file_type(user_client, create_task):
     doc_file = create_text_file('testfile')
     task = create_task
     data = {'doc_file': doc_file}
@@ -38,21 +37,18 @@ def test_03_task_doc_create_with_right_file_type(user_client,
 
 
 @pytest.mark.django_db()
-def test_04_task_doc_delete_file_with_object(user_client,
-                                             create_task):
+def test_04_task_doc_delete_file_with_object(user_client, create_task):
     doc_file = create_text_file('testfile')
     task = create_task
     data = {'doc_file': doc_file}
     user_client.post(f'/v1/tasks/task/{task["id"]}/doc/', data=data)
     doc_id = Doc.objects.all()[0].id
-    response = user_client.delete(
-        f'/v1/tasks/task/{task["id"]}/doc/{doc_id}/')
+    response = user_client.delete(f'/v1/tasks/task/{task["id"]}/doc/{doc_id}/')
     assert response.status_code == 204, f'{response.json()}'
 
 
 @pytest.mark.django_db()
-def test_05_task_doc_update_change_uploaded_file(user_client,
-                                                 create_task):
+def test_05_task_doc_update_change_uploaded_file(user_client, create_task):
     doc_file = create_text_file('testfile')
     task = create_task
     data = {'doc_file': doc_file}
@@ -77,106 +73,94 @@ def test_06_task_doc_create_with_wrong_file_type(user_client, create_task):
     image = create_image_file('small')
     task = create_task
     data = {'doc_file': image}
-    response = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/doc/', data=data)
+    response = user_client.post(f'/v1/tasks/task/{task["id"]}/doc/', data=data)
     assert response.status_code == 400, f'{response.json()}'
 
 
 @pytest.mark.django_db()
 def test_07_get_comment_docs_list(user_client, create_comment):
-    task, comment = create_comment
     response = user_client.get(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/')
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/')
     assert response.status_code == 200, f'{response.json()}'
 
 
 @pytest.mark.django_db()
 def test_08_get_comment_single_doc(user_client, create_comment):
-    task, comment = create_comment
     doc = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/',
         data={}).json()
     response = user_client.get(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/{doc["id"]}/')
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/{doc["id"]}/')
     assert response.status_code == 200, f'{response.json()}'
 
 
 @pytest.mark.django_db()
 def test_09_comment_doc_create_without_file(user_client, create_comment):
-    task, comment = create_comment
     data = {}
     response = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/',
         data=data)
     assert response.status_code == 201, f'{response.json()}'
 
 
 @pytest.mark.django_db()
-def test_10_comment_doc_create_with_right_file_type(user_client,
-                                                    create_comment):
+def test_10_comment_doc_create_with_right_file_type(user_client, create_comment):
     doc_file = create_text_file('testfile')
-    task, comment = create_comment
     data = {'doc_file': doc_file}
     response = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/',
         data=data)
     assert response.status_code == 201, f'{response.json()}'
-    filepath = f'media/files/{task["id"]}/testfile.pdf'
+    filepath = f'media/files/{create_comment["task"]}/testfile.pdf'
     assert os.path.isfile(filepath) is True
     doc_id = Doc.objects.all()[0].id
     user_client.delete(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/{doc_id}/')
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/{doc_id}/')
 
 
 @pytest.mark.django_db()
-def test_11_comment_doc_delete_file_with_object(user_client,
-                                                create_comment):
+def test_11_comment_doc_delete_file_with_object(user_client, create_comment):
     doc_file = create_text_file('testfile')
-    task, comment = create_comment
     data = {'doc_file': doc_file}
     response = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/',
         data=data)
     doc_id = Doc.objects.all()[0].id
     response2 = user_client.delete(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/{doc_id}/')
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/{doc_id}/')
     assert response2.status_code == 204, f'{response.json()}'
 
 
 @pytest.mark.django_db()
-def test_12_comment_doc_update_change_uploaded_file(user_client,
-                                                    create_comment):
+def test_12_comment_doc_update_change_uploaded_file(user_client, create_comment):
     doc_file = create_text_file('testfile')
-    task, comment = create_comment
     data = {'doc_file': doc_file}
     response = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/',
         data=data)
     assert response.status_code == 201, f'{response.json()}'
-    filepath = f'media/files/{task["id"]}/testfile.pdf'
+    filepath = f'media/files/{create_comment["task"]}/testfile.pdf'
     assert os.path.isfile(filepath) is True
     doc_file2 = create_text_file('testfile2')
     data2 = {'doc_file': doc_file2}
     doc_id = Doc.objects.all()[0].id
     response2 = user_client.patch(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/{doc_id}/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/{doc_id}/',
         data=data2)
     assert response2.status_code == 200, f'{response.json()}'
-    filepath2 = f'media/files/{task["id"]}/testfile2.pdf'
+    filepath2 = f'media/files/{create_comment["task"]}/testfile2.pdf'
     assert os.path.isfile(filepath2) is True
     response3 = user_client.delete(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/{doc_id}/')
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/{doc_id}/')
     assert response3.status_code == 204, f'{response.json()}'
     assert os.path.isfile(filepath) is False
 
 
 @pytest.mark.django_db()
-def test_13_comment_doc_create_with_wrong_file_type(user_client,
-                                                    create_comment):
+def test_13_comment_doc_create_with_wrong_file_type(user_client, create_comment):
     image = create_image_file('small')
-    task, comment = create_comment
     data = {'doc_file': image}
     response = user_client.post(
-        f'/v1/tasks/task/{task["id"]}/comment/{comment["id"]}/doc/',
+        f'/v1/tasks/task/{create_comment["task"]}/comment/{create_comment["id"]}/doc/',
         data=data)
     assert response.status_code == 400, f'{response.json()}'
--- wondertask/tests/test_07_observers.py
+++ wondertask/tests/test_07_observers.py
@@ -11,9 +11,9 @@ def test_01_get_task_observers_list(user_client, create_task):
 
 
 @pytest.mark.django_db()
-def test_02_get_task_single_observer(user_client, create_task):
+def test_02_get_task_single_observer(user_client, create_task, create_user):
     task = create_task
-    data = {"observer": 1}
+    data = {"observer": create_user.id}
     user_client.post(f'/v1/tasks/task/{task["id"]}/observers/', data=data)
     observer_id = Observer.objects.all()[0].id
     response = user_client.get(f'/v1/tasks/task/{task["id"]}/observers/{observer_id}/')
@@ -21,19 +21,18 @@ def test_02_get_task_single_observer(user_client, create_task):
 
 
 @pytest.mark.django_db()
-def test_03_task_observers_create(user_client, create_task):
+def test_03_task_observers_create(user_client, create_task, create_user):
     task = create_task
-    data = {"observer": 1}
+    data = {"observer": create_user.id}
     response = user_client.post(
         f'/v1/tasks/task/{task["id"]}/observers/', data=data)
     assert response.status_code == 201, f'{response.json()}'
 
 
 @pytest.mark.django_db()
-def test_04_task_observers_delete_file_with_object(user_client,
-                                                   create_task):
+def test_04_task_observers_delete_file_with_object(user_client, create_task, create_user):
     task = create_task
-    data = {"observer": 1}
+    data = {"observer": create_user.id}
     user_client.post(f'/v1/tasks/task/{task["id"]}/observers/', data=data)
     observer_id = Observer.objects.all()[0].id
     response = user_client.delete(
